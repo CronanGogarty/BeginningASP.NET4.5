a956ca7cd905ce359acea2e4362e06c45f1b8170
Cronán Gogarty <cronangogarty@outlook.com>
2016-05-18 18:47:16 -0700

    Caching

---
+++ Caching/CacheDependencies.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="Cache Dependencies" Language="C#" MasterPageFile="~/BasicTemplate.Master" AutoEventWireup="true" CodeBehind="CacheDependencies.aspx.cs" Inherits="BeginningASP.NET4._5.Caching.CacheDependencies" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="header" runat="server">
+    <h2>Cache Dependencies</h2>
+    <p>The XML file is stored in cache until it is altered.</p>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Button runat="server" ID="cmdAddToCache" Text="Add to Cache" OnClick="cmdAddToCache_Click" />
+    <br />
+    <asp:Button runat="server" ID="cmdQueryCache" Text="Query Cache" OnClick="cmdQueryCache_Click" />
+    <br />
+    <asp:Button runat="server" ID="cmdAlterXMLFile" Text="Alter XML File" OnClick="cmdAlterXMLFile_Click" />
+    <br />
+    <asp:Label runat="server" ID="lblResult"></asp:Label>
+    <br />
+    <asp:Literal runat="server" ID="xmlOut"></asp:Literal>
+</asp:Content>
+
---
+++ Caching/CacheDependencies.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Caching;
+using System.Xml;
+using System.IO;
+
+namespace BeginningASP.NET4._5.Caching
+{
+    public partial class CacheDependencies : System.Web.UI.Page
+    {
+        CacheDependency prodListDependency;
+        XmlDocument productList;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //create a dependency for the ProductList.xml file
+            prodListDependency = new CacheDependency(Server.MapPath("ProductList.xml"));
+            productList = new XmlDocument();
+            productList.Load(Server.MapPath("ProductList.xml"));
+
+            
+        }
+
+        protected void cmdAddToCache_Click(object sender, EventArgs e)
+        {
+            //add a cache item that is dependent on the ProductList.xml file
+            Cache.Insert("ProductList", productList, prodListDependency);
+            lblResult.Text += "<br />\"ProductList\" has been added to Cache";
+        }
+
+        protected void cmdQueryCache_Click(object sender, EventArgs e)
+        {
+            if (Cache["ProductList"] == null)
+            {
+                lblResult.Text += "<br />Cached item no longer exists";
+            }
+            else
+            {
+                XmlDocument cachedItem = (XmlDocument)Cache["ProductList"];
+                lblResult.Text += "<br />Retrieved \"ProductList\" from cache";
+                string theXml = Server.HtmlEncode(File.ReadAllText(Server.MapPath("ProductList.xml")));
+                xmlOut.Text = theXml;
+
+            }
+        }
+
+        protected void cmdAlterXMLFile_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlFile = new XmlDocument();
+            xmlFile.Load(Server.MapPath("ProductList.xml"));
+
+            XmlNode node;
+            node = xmlFile.DocumentElement;
+
+            foreach (XmlNode node1 in node.ChildNodes)
+            {
+                foreach (XmlNode node2 in node1.ChildNodes)
+                {
+                    if (node2.Name.ToUpper() == "COST")
+                    {
+                        try 
+	                    {
+                            decimal oldPrice = decimal.Parse(node2.InnerText);
+                            decimal newPrice = oldPrice + 1.99M;
+                            node2.InnerText = newPrice.ToString();
+	                    }
+	                    catch (Exception ex)
+	                    {
+                            System.Diagnostics.Debug.Print("There's been an EXCEPTION!!!" + ex.Message);
+	                    }
+                        
+                    }
+                }
+            }
+
+            xmlFile.Save(Server.MapPath("ProductList.xml"));
+        }
+    }
+}
