19f98313910310de9827695d5942dcdef5eb2400
Cronán Gogarty <cronangogarty@outlook.com>
2016-05-20 23:12:50 -0700

    LINQ to Entities

---
+++ EntityFramework/LINQToEntities.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="LINQ To Entities" Language="C#" MasterPageFile="~/BasicTemplate.Master" AutoEventWireup="true" CodeBehind="LINQToEntities.aspx.cs" Inherits="BeginningASP.NET4._5.EntityFramework.LINQToEntities" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="header" runat="server">
+    <h2>LINQ To Entities</h2>
+    <p>Using LINQ to Entities, you can write queries that act on your database.</p>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:GridView runat="server" ID="gridView1"></asp:GridView>
+    <br />
+    <asp:Button runat="server" ID="cmdHideGrid" Text="Hide Grid" OnClick="cmdHideGrid_Click"/>
+    <hr />
+    <asp:DropDownList runat="server" ID="ddlSelectCategory" AutoPostBack="true" OnSelectedIndexChanged="ddlSelectCategory_SelectedIndexChanged" AppendDataBoundItems="true">
+        <asp:ListItem Value="-1" Text="Select a Category"></asp:ListItem>
+    </asp:DropDownList>
+    <br />
+    <asp:GridView runat="server" ID="gridViewCategoryProducts"></asp:GridView>
+    <br />
+    <asp:Label runat="server" ID="lblErrorMessage"></asp:Label>
+</asp:Content>
+
---
+++ EntityFramework/LINQToEntities.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace BeginningASP.NET4._5.EntityFramework
+{
+    public partial class LINQToEntities : System.Web.UI.Page
+    {
+        NorthwindEntities1 entities;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            entities = new NorthwindEntities1();
+
+            if (!Page.IsPostBack)
+            {
+                try
+                {
+                    GetOrdersByID("VAFFE");
+                    GetCategories();
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.Print(ex.Message);
+                    lblErrorMessage.Text = ex.Message;
+                }
+            }
+        }
+
+        private void GetOrdersByID(string id)
+        {
+            gridView1.DataSource = from orders in entities.Orders.ToList()
+                                   where orders.CustomerID == id
+                                   select orders;
+
+            gridView1.DataBind();
+        }
+
+        private void GetCategories()
+        {
+            ddlSelectCategory.DataTextField = "CategoryName";
+            ddlSelectCategory.DataValueField = "CategoryID";
+            ddlSelectCategory.DataSource = entities.Categories.ToList();
+            ddlSelectCategory.DataBind();
+        }
+
+        protected void ddlSelectCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = Int32.Parse(ddlSelectCategory.SelectedValue);
+            if (selectedIndex > -1)
+            {
+                try
+                {
+                    PopulateCategoryProducts(selectedIndex);
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.Print(ex.Message);
+                    lblErrorMessage.Text = ex.Message;
+                }
+            }
+        }
+
+        private void PopulateCategoryProducts(int id)
+        {
+            gridViewCategoryProducts.DataSource = from product in entities.Products.ToList()
+                                                  where product.CategoryID == id
+                                                  select new
+                                                      {
+                                                          ID = product.ProductID,
+                                                          Name = product.ProductName,
+                                                          UnitPrice = product.UnitPrice,
+                                                          InStock = product.UnitsInStock
+                                                      };
+
+            gridViewCategoryProducts.DataBind();
+        }
+
+        protected void cmdHideGrid_Click(object sender, EventArgs e)
+        {
+            gridView1.Visible = false;
+        }
+    }
+}
